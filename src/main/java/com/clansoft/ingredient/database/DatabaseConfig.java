21b2fb42301d1360e5fbf0a9157e9260403f9996
Chuck Lan <chuck.lan@texture.com>
2019-12-11 16:30:05 -0800

    get datasource info

--- src/main/java/com/clansoft/ingredient/database/DatabaseConfig.java
+++ src/main/java/com/clansoft/ingredient/database/DatabaseConfig.java
@@ -1,12 +1,17 @@
 package com.clansoft.ingredient.database;
 
 import com.zaxxer.hikari.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
+	
+  Logger logger = LoggerFactory.getLogger(DatabaseConfig.class);
 
   @Value("${spring.datasource.url}")
   private String dbUrl;
@@ -21,8 +26,11 @@ public class DatabaseConfig {
   public DataSource dataSource() {
       HikariConfig config = new HikariConfig();
       config.setJdbcUrl(dbUrl);
+      logger.info("datasource url : {}", dbUrl);
       config.setUsername(username);
+      logger.info("datasource user name : {}", username);
       config.setPassword(password);
+      logger.info("datasource password : {}", password);
       return new HikariDataSource(config);
   }
 }
